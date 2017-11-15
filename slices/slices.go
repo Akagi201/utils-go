945cbf415bd246bccab23ba84350852a0aaf89cc
Akagi201 <akagi201@gmail.com>
2017-11-15 14:04:36 +0800

    feat(slices): add struct field to slice

--- slices/slices.go
+++ slices/slices.go
@@ -3,6 +3,26 @@ package slices
 
 import (
 	"reflect"
+
+	"github.com/fatih/structs"
+	"github.com/pkg/errors"
+)
+
+var (
+	// ErrNotSlice happens when the value passed is not a slice.
+	ErrNotSlice = errors.New("not slice")
+
+	// ErrNotString happens when the value of the field is not a string.
+	ErrNotString = errors.New("not string")
+
+	// ErrNotInt happens when the value of the field is not an int.
+	ErrNotInt = errors.New("not int")
+
+	// ErrNotFloat happens when the value of the field is not a float64.
+	ErrNotFloat = errors.New("not float64")
+
+	// ErrNotBool happens when the value of the field is not a bool.
+	ErrNotBool = errors.New("not bool")
 )
 
 // IndexOf returns the first index at which a given element can be found in the slice, or -1 if it is not present.
@@ -24,4 +44,132 @@ func IndexOf(val interface{}, slice interface{}) int {
 	}
 
 	return -1
+}
+
+// ToStrings maps a field to a slice of string.
+func ToStrings(slice interface{}, fieldName string) (s []string, err error) {
+	switch reflect.TypeOf(slice).Kind() {
+	case reflect.Slice:
+		val := reflect.ValueOf(slice)
+		for i := 0; i < val.Len(); i++ {
+			fields := structs.Fields(val.Index(i).Interface())
+			for _, f := range fields {
+				if f.IsExported() && f.Name() == fieldName {
+					e := f.Value()
+					switch e.(type) {
+					case string:
+						s = append(s, e.(string))
+					default:
+						return nil, ErrNotString
+					}
+				}
+			}
+		}
+	default:
+		return nil, ErrNotSlice
+	}
+	return
+}
+
+// ToInts maps a field to a slice of int.
+func ToInts(slice interface{}, fieldName string) (s []int, err error) {
+	switch reflect.TypeOf(slice).Kind() {
+	case reflect.Slice:
+		val := reflect.ValueOf(slice)
+		for i := 0; i < val.Len(); i++ {
+			fields := structs.Fields(val.Index(i).Interface())
+			for _, f := range fields {
+				if f.IsExported() && f.Name() == fieldName {
+					e := f.Value()
+					switch e.(type) {
+					case int:
+						s = append(s, e.(int))
+					default:
+						return nil, ErrNotInt
+					}
+				}
+			}
+		}
+	default:
+		return nil, ErrNotSlice
+	}
+	return
+}
+
+// ToFloats maps a field to a slice of int.
+func ToFloats(slice interface{}, fieldName string) (s []float64, err error) {
+	switch reflect.TypeOf(slice).Kind() {
+	case reflect.Slice:
+		val := reflect.ValueOf(slice)
+		for i := 0; i < val.Len(); i++ {
+			fields := structs.Fields(val.Index(i).Interface())
+			for _, f := range fields {
+				if f.IsExported() && f.Name() == fieldName {
+					e := f.Value()
+					switch e.(type) {
+					case float64:
+						s = append(s, e.(float64))
+					default:
+						return nil, ErrNotFloat
+					}
+				}
+			}
+		}
+	default:
+		return nil, ErrNotSlice
+	}
+	return
+}
+
+// ToBools maps a field to a slice of bool.
+func ToBools(slice interface{}, fieldName string) (s []bool, err error) {
+	switch reflect.TypeOf(slice).Kind() {
+	case reflect.Slice:
+		val := reflect.ValueOf(slice)
+		for i := 0; i < val.Len(); i++ {
+			fields := structs.Fields(val.Index(i).Interface())
+			for _, f := range fields {
+				if f.IsExported() && f.Name() == fieldName {
+					e := f.Value()
+					switch e.(type) {
+					case bool:
+						s = append(s, e.(bool))
+					default:
+						return nil, ErrNotBool
+					}
+				}
+			}
+		}
+	default:
+		return nil, ErrNotSlice
+	}
+	return
+}
+
+// ToStringsUnsafe maps a field to a slice of string but not returns an error.
+// If an error still happens, s will be nil.
+func ToStringsUnsafe(slice interface{}, fieldName string) (s []string) {
+	s, _ = ToStrings(slice, fieldName)
+	return
+}
+
+// ToIntsUnsafe maps a field to a slice of int but not returns an error.
+// If an error still happens, s will be nil.
+func ToIntsUnsafe(slice interface{}, fieldName string) (s []int) {
+	s, _ = ToInts(slice, fieldName)
+	return
+}
+
+// ToFloatsUnsafe maps a field to a slice of float but not returns an error.
+// If an error still happens, s will be nil.
+func ToFloatsUnsafe(slice interface{}, fieldName string) (s []float64) {
+	s, _ = ToFloats(slice, fieldName)
+	return
+}
+
+// ToBoolsUnsafe maps a field to a slice of bool but not returns an error.
+// If an error still happens, s will be nil.
+func ToBoolsUnsafe(slice interface{}, fieldName string) (s []bool) {
+	s, _ = ToBools(slice, fieldName)
+	return
 }
--- slices/slices_test.go
+++ slices/slices_test.go
@@ -75,3 +75,132 @@ func ExampleIndexOf() {
 	// 2
 	// -1
 }
+
+type Person struct {
+	ID    int
+	Name  string
+	Money float64
+	Male  bool
+}
+
+type Group struct {
+	People []*Person
+}
+
+func TestMapSliceToString(t *testing.T) {
+	assert := assert.New(t)
+
+	g := &Group{
+		People: []*Person{
+			{0, "George", 42.42, true},
+			{1, "Jeff", 0, true},
+			{2, "Ted", 50, true},
+			{3, "Luda", 100, false},
+		},
+	}
+	s, err := slices.ToStrings(g.People, "Name")
+	assert.Nil(err)
+	assert.NotEmpty(s)
+	assert.Equal(s[0], "George")
+	assert.Equal(s[1], "Jeff")
+	assert.Equal(s[2], "Ted")
+	assert.Equal(s[3], "Luda")
+
+	s, err = slices.ToStrings(g.People, "ID")
+	assert.Equal(slices.ErrNotString, err)
+}
+
+func TestMapSliceToInt(t *testing.T) {
+	assert := assert.New(t)
+
+	g := &Group{
+		People: []*Person{
+			{0, "George", 42.42, true},
+			{1, "Jeff", 0, true},
+			{2, "Ted", 50, true},
+			{3, "Luda", 100, false},
+		},
+	}
+
+	s, err := slices.ToInts(g.People, "ID")
+	assert.Nil(err)
+	assert.NotEmpty(s)
+	assert.Equal(s[0], 0)
+	assert.Equal(s[1], 1)
+	assert.Equal(s[2], 2)
+	assert.Equal(s[3], 3)
+
+	s, err = slices.ToInts(g.People, "Money")
+	assert.Equal(slices.ErrNotInt, err)
+}
+
+func TestMapSliceToFloat(t *testing.T) {
+	assert := assert.New(t)
+
+	g := &Group{
+		People: []*Person{
+			{0, "George", 42.42, true},
+			{1, "Jeff", 0, true},
+			{2, "Ted", 50, true},
+			{3, "Luda", 100, false},
+		},
+	}
+
+	s, err := slices.ToFloats(g.People, "Money")
+	assert.Nil(err)
+	assert.NotEmpty(s)
+	assert.Equal(s[0], 42.42)
+	assert.Equal(s[1], 0.0)
+	assert.Equal(s[2], 50.0)
+	assert.Equal(s[3], 100.0)
+
+	s, err = slices.ToFloats(g.People, "ID")
+	assert.Equal(slices.ErrNotFloat, err)
+}
+
+func TestMapSliceToBool(t *testing.T) {
+	assert := assert.New(t)
+
+	g := &Group{
+		People: []*Person{
+			{0, "George", 42.42, true},
+			{1, "Jeff", 0, true},
+			{2, "Ted", 50, true},
+			{3, "Luda", 100, false},
+		},
+	}
+
+	s, err := slices.ToBools(g.People, "Male")
+	assert.Nil(err)
+	assert.NotEmpty(s)
+	assert.Equal(s[0], true)
+	assert.Equal(s[1], true)
+	assert.Equal(s[2], true)
+	assert.Equal(s[3], false)
+
+	s, err = slices.ToBools(g.People, "Name")
+	assert.Equal(slices.ErrNotBool, err)
+}
+
+type Tag struct {
+	Name string
+}
+
+type Post struct {
+	Title string
+	Tags  []*Tag
+}
+
+func Example() {
+	post := &Post{
+		Title: "GOLANG",
+		Tags: []*Tag{
+			{"Go"}, {"Golang"}, {"Gopher"},
+		},
+	}
+	s, _ := slices.ToStrings(post.Tags, "Name")
+	fmt.Println(s)
+
+	// Output:
+	// [Go Golang Gopher]
+}
