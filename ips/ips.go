9581262f7c56f4967bf14c161fc59a53db39ead5
Akagi201 <akagi201@gmail.com>
2018-01-11 11:00:47 +0800

    feat(ips): add GetIPv4ByInterface

--- ips/ips.go
+++ ips/ips.go
@@ -1,14 +1,39 @@
 // Package ips provides ip helper functions
 package ips
 
-import "net"
+import (
+	"net"
+)
 
-// LocalIPs return all non-loopback IP addresses
-func LocalIPs() ([]string, error) {
+// LocalIPs return all non-loopback IPv4 addresses
+func LocalIPv4s() ([]string, error) {
 	var ips []string
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
 		return ips, err
+	}
+
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
+			ips = append(ips, ipnet.IP.String())
+		}
+	}
+
+	return ips, nil
+}
+
+// GetIPv4ByInterface return IPv4 address from a specific interface IPv4 addresses
+func GetIPv4ByInterface(name string) ([]string, error) {
+	var ips []string
+
+	iface, err := net.InterfaceByName(name)
+	if err != nil {
+		return nil, err
+	}
+
+	addrs, err := iface.Addrs()
+	if err != nil {
+		return nil, err
 	}
 
 	for _, a := range addrs {
